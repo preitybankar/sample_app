b6808d073a5e5cb0d30dec2ef133a966e0de3d26
priti <preity.bankar@gmail.com>
2012-09-21 18:30:10 +0530

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -22,7 +22,15 @@ describe "User pages" do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
+    
+    describe "after submission" do
+        before { click_button submit }
 
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
+
+    
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
@@ -37,6 +45,15 @@ describe "User pages" do
     end
   end
   
+  describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
